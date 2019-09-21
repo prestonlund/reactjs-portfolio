f7322484961f37b70b8e6c71514e234ed3737ba3
Preston Lund <prestonlund@gmail.com>
2019-09-21 16:36:53 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import prestonprofile from '../assets/prestonprofile.jpg';
+
+class App extends Component {
+    state = { displayBio: false };
+
+    toggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={prestonprofile} alt='profile' className='profile' />
+                <h1>Hello!</h1>
+                <p>My name is Preston. I'm a front end developer.</p>
+                <p>I'm always looking forward to working on meaningful projects that challenge me.</p>
+                <p>I developed this portfolio using React.js.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Salt Lake City, and code every day.</p>
+                            <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+                            <p>Besides coding, I also love playing the guitar with my wife and daughters.</p>
+                            <button onClick={this.toggleDisplayBio}>Show less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+                }
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:prestonlund@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/prestonlund',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/preston-lund-95a7493/',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        link: 'https://twitter.com/preston8289',
+        image: twitterIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
