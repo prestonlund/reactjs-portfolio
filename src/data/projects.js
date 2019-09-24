bd32ec13033b1193fdc958736fabbf1c951513e1
Preston Lund <prestonlund@gmail.com>
2019-09-23 21:51:54 -0600

    add photos to portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-import project1 from '../assets/project1.png';
+import project1 from '../assets/yelp-camp-thumbnail-react.png';
 import project2 from '../assets/project2.png';
 import project3 from '../assets/project3.png';
 
